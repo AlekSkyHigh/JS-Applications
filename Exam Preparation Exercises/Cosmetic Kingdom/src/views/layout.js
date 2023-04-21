ce8e10458cc0ac22f8fb0ebf0dc9cf4f575ad36e
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2023-04-21 12:27:09 +0300

    Create layout.js

---
+++ Exam Preparation Exercises/Cosmetic Kingdom/src/views/layout.js
@@ -0,0 +1,31 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
+// TODO replace with actual layout
+export const layoutTemplate = (userData, content) => html`
+ <header>
+        <!-- Navigation -->
+        <a id="logo" href="/"
+          ><img id="logo-img" src="./images/logo.png" alt=""
+        /></a>
+
+        <nav>
+          <div>
+            <a href="/dashboard">Products</a>
+          </div>
+            ${userData
+            ? html `<!-- Logged-in users -->
+          <div class="user">
+            <a href="/create">Add Product</a>
+            <a href="/logout">Logout</a>
+          </div>` 
+            : html `<!-- Guest users -->
+          <div class="guest">
+            <a href="/login">Login</a>
+            <a href="/register">Register</a>
+          </div>`}
+        </nav>
+      </header>
+
+<main>
+    ${content}
+</main>`; 
