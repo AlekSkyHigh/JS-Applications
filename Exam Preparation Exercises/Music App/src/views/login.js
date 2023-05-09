c230cc5e67758cee9f343bfa4615b85869135aaf
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2023-05-09 14:04:39 +0300

    Create login.js

---
+++ Exam Preparation Exercises/Music App/src/views/login.js
@@ -0,0 +1,37 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { login } from '../data/auth.js';
+import { createSubmitHandler } from '../util.js';
+
+// TODO Replace with actual view
+const loginTemplate = (onLogin) => html`
+<section id="loginPage">
+    <form @submit=${onLogin}>
+        <fieldset>
+            <legend>Login</legend>
+
+            <label for="email" class="vhide">Email</label>
+            <input id="email" class="email" name="email" type="text" placeholder="Email">
+
+            <label for="password" class="vhide">Password</label>
+            <input id="password" class="password" name="password" type="password" placeholder="Password">
+
+            <button type="submit" class="login">Login</button>
+
+            <p class="field">
+                <span>If you don't have profile click <a href="/register">here</a></span>
+            </p>
+        </fieldset>
+    </form>
+</section>`;
+
+export function loginPage(ctx) {
+    ctx.render(loginTemplate(createSubmitHandler(onLogin)));
+
+    // TODO change user object based on requirements
+    async function onLogin({ email, password }, form) {
+        await login(email, password);
+        form.reset();
+        // TODO use redicrect location from requirements
+        ctx.page.redirect('/');
+    }
+}
