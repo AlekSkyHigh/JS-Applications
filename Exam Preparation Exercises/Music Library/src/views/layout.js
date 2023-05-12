191b77d9b93c8c9118efe0c71b23333efdc31a12
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2023-05-12 14:39:10 +0300

    Create layout.js

---
+++ Exam Preparation Exercises/Music Library/src/views/layout.js
@@ -0,0 +1,38 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
+// TODO replace with actual layout
+export const layoutTemplate = (userData, content) => html`
+<header>
+    <!-- Navigation -->
+    <a id="logo" href="/"><img id="logo-img" src="/images/logo.png" alt="" /></a>
+
+    <nav>
+        <div>
+            <a href="/dashboard">Dashboard</a>
+        </div>
+
+        ${userData 
+        ? html` <!-- Logged-in users -->
+        <div class="user">
+            <a href="/create">Add Album</a>
+            <a href="/logout">Logout</a>
+        </div>` 
+        : html ` <!-- Guest users -->
+        <div class="guest">
+            <a href="/login">Login</a>
+            <a href="/register">Register</a>
+        </div>
+        `}
+        
+    </nav>
+</header>
+
+<main>
+    ${content}
+</main>
+
+<footer>
+    <p>@MusicLibrary</p>
+</footer>
+
+`; 
