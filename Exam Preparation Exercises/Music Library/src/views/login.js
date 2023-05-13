641f1e931fb9ee94b846a0d3b73361ad0b882c9a
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2023-05-13 12:34:33 +0300

    Create login.js

---
+++ Exam Preparation Exercises/Music Library/src/views/login.js
@@ -0,0 +1,35 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { login } from '../data/auth.js';
+import { createSubmitHandler } from '../util.js';
+
+// TODO Replace with actual view
+const loginTemplate = (onLogin) => html`
+<section id="login">
+        <div class="form">
+          <h2>Login</h2>
+          <form @submit=${onLogin} class="login-form">
+            <input type="text" name="email" id="email" placeholder="email" />
+            <input type="password" name="password" id="password" placeholder="password" />
+            <button type="submit">login</button>
+            <p class="message">
+              Not registered? <a href="/register">Create an account</a>
+            </p>
+          </form>
+        </div>
+      </section>`;
+
+export function loginPage(ctx){
+    ctx.render(loginTemplate(createSubmitHandler(onLogin)));
+
+    // TODO change user object based on requirements
+    async function onLogin({email, password}, form){
+        if (email === '' || password === '') {
+            return alert('Please add email and password');
+        }
+        await login(email, password);
+        form.reset();
+        // TODO use redicrect location from requirements
+        ctx.page.redirect('/');
+    }
+}
+ 
