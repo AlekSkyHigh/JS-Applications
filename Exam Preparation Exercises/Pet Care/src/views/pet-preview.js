8bb1b21fa2340acdbec2c83f579d264f6ec625db
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2023-05-17 21:07:49 +0300

    Create pet-preview.js

---
+++ Exam Preparation Exercises/Pet Care/src/views/pet-preview.js
@@ -0,0 +1,14 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+
+export const petPreview = (pet) => html`
+<div class="animals-board">
+    <article class="service-img">
+        <img class="animal-image-cover" src="${pet.image}">
+    </article>
+    <h2 class="name">${pet.name}</h2>
+    <h3 class="breed">${pet.breed}</h3>
+    <div class="action">
+        <a class="btn" href="/details/${pet._id}">Details</a>
+    </div>
+</div>
+`
