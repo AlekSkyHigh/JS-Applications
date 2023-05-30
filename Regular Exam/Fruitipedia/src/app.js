8beae6840948028ca355fd0bb6557df911eacf8c
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2023-05-30 18:53:03 +0300

    Create app.js

---
+++ Regular Exam/Fruitipedia/src/app.js
@@ -0,0 +1,47 @@
+import page from '../node_modules/page/page.mjs';
+import {render} from '../node_modules/lit-html/lit-html.js';
+import { layoutTemplate } from './views/layout.js';
+import { getUserData } from './util.js';
+import { homePage } from './views/home.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
+import { logout } from './data/auth.js';
+import { dashboardPage } from './views/dashboard.js';
+import { addFruitPage } from './views/create.js';
+import { detailsPage } from './views/details.js';
+import { editPage } from './views/edit.js';
+import { searchPage } from './views/search.js';
+
+// TODO change render root depending on project HTML structure
+const root = document.getElementById('wrapper');
+
+page(decorateContext);
+page('index.html', '/');
+page('/', homePage);
+page('/login', loginPage);
+page('/register', registerPage);
+page('/logout', logoutAction);
+page('/dashboard', dashboardPage);
+page('/create', addFruitPage);
+page('/details/:id', detailsPage);
+page('/edit/:id', editPage);
+page('/search', searchPage);
+
+page.start();
+
+function decorateContext(ctx, next){
+    ctx.render = renderView;
+
+    next();
+}
+
+// TODO inject dependencies
+function renderView(content){
+    const userData = getUserData();
+    render(layoutTemplate(userData, content), root);
+}
+
+function logoutAction(ctx) {
+    logout();
+    ctx.page.redirect('/');
+}
